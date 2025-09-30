a62e12c32c76bdd001ba41cc64f8be1f6bcd2ecb
Ryder Baez <rbbaez@usc.edu>
2025-09-30 14:41:04 -0700

    complete

--- race.c
+++ race.c
@@ -1,13 +1,24 @@
 #include "types.h"
 #include "stat.h"
 #include "user.h"
+#include "condvar.h"
+#include "fcntl.h"
+
 int main(){
+    struct condvar cv;
+    init_lock(&cv.lk);
     int pid = fork();
+    int fd = open("flag", O_RDWR | O_CREATE);
     if(pid < 0){
         printf(1, "Error forking first child.\n");
     }
     else if (pid == 0){
+        sleep(5);
         printf(1, "Child 1 Executing\n");
+        lock(&cv.lk);
+        write(fd, "done", 4);
+        cv_signal(&cv);
+        unlock(&cv.lk);
     }
     else{
         pid = fork();
@@ -15,6 +26,17 @@ int main(){
             printf(1, "Error forking second child.\n");
         }
         else if (pid == 0){
+            lock(&cv.lk);
+            struct stat stats;
+fstat(fd, &stats);
+printf(1, "file size = %d\n", stats.size);
+while(stats.size <= 0){
+cv_wait(&cv);
+fstat(fd, &stats);
+printf(1, "file size = %d\n", stats.size);
+}
+
+        unlock(&cv.lk);
         printf(1, "Child 2 Executing\n");
     }
     else{
@@ -29,4 +51,6 @@ int main(){
     }
     exit();
     }
+    close(fd);
+unlink("flag");
 }
